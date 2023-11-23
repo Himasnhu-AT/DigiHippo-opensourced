15484b56bb4460c43e6ca4e36642a696ba68c08f
Himanshu-AT <aristech.official@gmail.com>
2023-11-23 19:11:05 +0530

    component design

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,21 +1,22 @@
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils'
+import { ReactNode } from 'react'
 
 const MaxWidthWrapper = ({
-    classname,
-    children
+  className,
+  children,
 }: {
-    classname?: string,
-    children: React.ReactNode
+  className?: string
+  children: ReactNode
 }) => {
-    return (
-        <div className={
-            cn("mx-auto w-full max-w-screen-x1 px-2.5 md:px-20",
-                classname
-            )}
-        >
-            {children}
-        </div>
-    )
+  return (
+    <div
+      className={cn(
+        'mx-auto w-full max-w-screen-xl px-2.5 md:px-20',
+        className
+      )}>
+      {children}
+    </div>
+  )
 }
 
-export default MaxWidthWrapper;
+export default MaxWidthWrapper
