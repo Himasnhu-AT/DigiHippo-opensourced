b8a3c27bd11e7ac38ad674896f57e3d3e7ed5d3d
Himanshu-AT <aristech.official@gmail.com>
2023-11-23 19:08:40 +0530

    auth :)

---
+++ src/collections/Users.ts
@@ -0,0 +1,39 @@
+import { CollectionConfig } from "payload/types";
+
+export const Users: CollectionConfig = {
+    slug: 'users',
+    auth: {
+        verify: {
+            generateEmailHTML: ({ token }) => {
+                return `<a href="${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}">Verify Email</a>`
+
+            },
+        },
+    },
+    access: {
+        read: () => true,
+        create: () => true,
+    },
+    fields: [
+        {
+            name: 'role',
+            defaultValue: 'user',
+            required: true,
+            admin: {
+                condition: ({ req }) => req.user.role === 'admin',
+            },
+            type: "select",
+            options: [
+                {
+                    label: 'User',
+                    value: 'user',
+                },
+                {
+                    label: 'Admin',
+                    value: 'admin',
+                },
+            ],
+
+        }
+    ],
+}
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 import { getPayloadClient } from './get-payload'
 import { nextApp, nextHandler } from './next-utils'
+import * as trpcExpress from '@trpc/server/adapters/express'
 import { appRouter } from './trpc'
+import { inferAsyncReturnType } from '@trpc/server'
 import bodyParser from 'body-parser'
 import { IncomingMessage } from 'http'
+// import { stripeWebhookHandler } from './webhooks'
 import nextBuild from 'next/dist/build'
 import path from 'path'
 import { PayloadRequest } from 'payload/types'
@@ -12,8 +15,34 @@ import { parse } from 'url'
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
 
+const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({
+  req,
+  res,
+})
+
+export type ExpressContext = inferAsyncReturnType<
+  typeof createContext
+>
+
+export type WebhookRequest = IncomingMessage & {
+  rawBody: Buffer
+}
 
 const start = async () => {
+  const webhookMiddleware = bodyParser.json({
+    verify: (req: WebhookRequest, _, buffer) => {
+      req.rawBody = buffer
+    },
+  })
+
+  app.post(
+    '/api/webhooks/stripe',
+    webhookMiddleware,
+    // stripeWebhookHandler
+  )
 
   const payload = await getPayloadClient({
     initOptions: {
@@ -38,6 +67,31 @@ const start = async () => {
 
     return
   }
+
+  const cartRouter = express.Router()
+
+  cartRouter.use(payload.authenticate)
+
+  cartRouter.get('/', (req, res) => {
+    const request = req as PayloadRequest
+
+    if (!request.user)
+      return res.redirect('/sign-in?origin=cart')
+
+    const parsedUrl = parse(req.url, true)
+    const { query } = parsedUrl
+
+    return nextApp.render(req, res, '/cart', query)
+  })
+
+  app.use('/cart', cartRouter)
+  app.use(
+    '/api/trpc',
+    trpcExpress.createExpressMiddleware({
+      router: appRouter,
+      createContext,
+    })
+  )
 
   app.use((req, res) => nextHandler(req, res))
 
